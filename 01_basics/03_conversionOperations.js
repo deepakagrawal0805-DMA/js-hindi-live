909d6c79c8b1de8a955f92eb9082e872ee55cf13
deepakagrawal0805-DMA <deepakagrawal0805@gmail.com>
2026-06-27 03:50:47 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,28 @@
+let score = "deepak"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+//  "48" => 48
+// "48abcd" => NaN (Not a Number)
+// true => 1; false => 0
+
+let isLoggedIn = "deepak"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false; 
+// "deepak" => true
+
+let someNumber = 55
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
