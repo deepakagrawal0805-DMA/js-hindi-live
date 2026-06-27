a26665d726eed55a164c9167eb1bc59a89b3063c
deepakagrawal0805-DMA <deepakagrawal0805@gmail.com>
2026-06-27 05:01:36 +0000

    datatype summary

---
+++ datatypes-summery.js
@@ -0,0 +1,37 @@
+// # Primitive
+
+// 7 types : string, number, bigint, boolean, undefined, symbol, null
+
+
+const score = 100
+const scoreValue = 100.4
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 34567890123456789012345678901234567890n
+
+
+// Reference(None primitive)
+
+// Array, Object, Function
+
+const heros = ['shaktiman', 'hatimtai', 'sonpari']
+let myObj = {
+    name: "deepak",
+    age: 23,
+
+}
+
+const myFunction = function(){
+    console.log("hello world");
+}
+
+console.log(typeof anotherId);
+
