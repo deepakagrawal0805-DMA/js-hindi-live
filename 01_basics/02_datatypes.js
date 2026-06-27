7dd065b01b9db36c5f4dc72bac292e966765f518
deepakagrawal0805-DMA <deepakagrawal0805@gmail.com>
2026-06-27 03:15:02 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    + 
+    3) // code readability should be prioritized over saving a few lines of code
+
+    console.log("deepak")
+
+
+    let name = "deepak" // string
+    let age = 23 // number
+    let isLoggedin = false // boolean
+    let state;
+
+
+    //  number => 2 to power of 53
+    //  bigint
+    //  boolean => true/false
+    // null => standalone value
+    // undefined => value is not assigned
+    // symbol => unique identifiers for objects
+
+
+    // objects => key value pairs
+
+    console.log(typeof "deepak") // string
+    console.log(typeof 23) // number
+    console.log(typeof true) // boolean
+    console.log(typeof undefined) // undefined
+    console.log(typeof null) // object
