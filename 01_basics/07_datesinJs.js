161aa03971293cfe2cbbab897d0ac64fe00400e4
deepakagrawal0805-DMA <deepakagrawal0805@gmail.com>
2026-06-29 03:28:12 +0000

    datesinJs

---
+++ 01_basics/07_datesinJs.js
@@ -0,0 +1,32 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// // console.log(myDate.toLocaleString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+//  let myCreatedDate = new Date(2025, 0, 25)
+//   let myCreatedDate = new Date(2025, 0, 25, 5, 4)
+// let myCreatedDate = new Date("2025-01-25")
+let myCreatedDate = new Date("05-08-2025")
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+
+// '${newDate.getDay()} and the time '
+
+newDate.toLocaleString('default', {
+    weekday: 'long'
+})
